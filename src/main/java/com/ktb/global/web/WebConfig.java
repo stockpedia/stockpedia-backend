cb004a42842dde180a00cd179c23c14c833da19a
김남욱 <wsh6922@naver.com>
2026-08-04 10:04:27 +0900

    [feat]: umbrella-helm > values.yaml, web > WebConfig.java 변경

--- src/main/java/com/ktb/global/web/WebConfig.java
+++ src/main/java/com/ktb/global/web/WebConfig.java
@@ -1,7 +1,10 @@
 package com.ktb.global.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +12,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.nio.file.Paths;
 
 @Configuration
-
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${file.upload-path}")
@@ -18,15 +20,28 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedOrigins(
+                        "https://www.shiftknu.com",
+                        "https://api.shiftknu.com",
+                        "http://localhost:3000"
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("SESSION");
+        serializer.setUseHttpOnlyCookie(true);
+        serializer.setUseSecureCookie(true);
+        serializer.setSameSite("None");
+        return serializer;
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
         String uploadPath = Paths.get(basePath)
                 .toAbsolutePath()
                 .toUri()
